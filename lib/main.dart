4557f8ca18b18828ec1bdf3fdd6443cb2a3d74b6
devkabiir <dev@kabiir.me>
2018-10-01 21:48:30 +0530

    :lipstick: (app-bar) update appbar to look like the mockup

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,8 @@ class ChatListApp extends StatelessWidget {
     return MaterialApp(
       title: 'Chat List Demo',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
-        accentColor: Colors.tealAccent,
+        primarySwatch: Colors.green,
+        accentColor: Colors.green,
       ),
       home: const _MyHomePage(title: 'Chat List'),
     );
@@ -31,8 +31,55 @@ class _MyHomePageState extends State<_MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 8.0,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(95.0),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+            child: TextField(
+              controller: TextEditingController(),
+              decoration: InputDecoration(
+                  isDense: true,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 25.0),
+                  hintText: 'Search your Friends...',
+                  suffixIcon: Padding(
+                    padding: const EdgeInsetsDirectional.only(end: 12.0),
+                    child: Icon(
+                      Icons.search,
+                      size: 30.0,
+                    ), // icon is 48px widget.
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(60.0),
+                  ),
+                  filled: true),
+            ),
+          ),
+        ),
+        leading: Icon(
+          Icons.arrow_back_ios,
+          color: Colors.black87,
+        ),
+        backgroundColor: Colors.white,
+        actions: <Widget>[
+          FloatingActionButton(
+            onPressed: () {},
+            mini: true,
+            backgroundColor: Colors.greenAccent[400],
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+              size: 20.0,
+            ),
+          )
+        ],
         title: Text(
           widget.title,
+          style: TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.black87,
+          ),
         ),
       ),
       body: Center(
@@ -41,11 +88,11 @@ class _MyHomePageState extends State<_MyHomePage> {
           children: <Widget>[],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      //   floatingActionButton: FloatingActionButton(
+      //     onPressed: () {},
+      //     tooltip: 'Increment',
+      //     child: const Icon(Icons.add),
+      //   ),
     );
   }
 }
